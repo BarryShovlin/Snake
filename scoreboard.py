717d2378b964aea4026c120af40c51cc5cf1e851
BarryShovlin <bshovlin32@gmail.com>
2023-05-26 22:33:46 -0500

    added read and write functionality in scorebaord.py  to a txt file to keep a running high score

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,16 @@
 from turtle import Turtle, Screen
 
+with open('data.txt', mode="r") as high_score:
+    hs = high_score.read()
+    high_score = int(hs)
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.penup()
         self.goto(0, 270)
@@ -18,7 +23,8 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.score))
         self.score = 0
         self.show_score()
 
